bdb67b85fd0d2e1567335eac2943a36cd9ba8831
David Arenzana <davidarenzana@calidae.com>
2026-04-19 12:10:17 +0200

    fix(components): fix button gradient hover effect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   theme: {
     extend: {
+      colors: {
+        fucsia: '#e175ea',
+        cyan: '#8cf1ff',
+      },
       backgroundImage: {
-        "gradient-brand":
-          "linear-gradient(to right, #E175EA 12.77%, #8CF1FF 114.5%)",
-        "gradient-border":
-          "linear-gradient(#2a2735 0 0) padding-box, linear-gradient(to right, #E175EA 12.77%, #8CF1FF 114.5%) border-box",
+        'gradient-brand': 'linear-gradient(to right, #E175EA 12.77%, #8CF1FF 114.5%)',
+        'gradient-border':
+          'linear-gradient(#2a2735 0 0) padding-box, linear-gradient(to right, #E175EA 12.77%, #8CF1FF 114.5%) border-box',
       },
     },
   },
-} satisfies Config;
+} satisfies Config
